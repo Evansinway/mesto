8b663c96d7f0e51c0428144bdeeb5f69ebdf034f
Natalya Evans <nvshantal@gmail.com>
2023-02-17 11:59:01 +0500

    ПР5 Второе ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
-const popup = document.querySelector('.popup');  //ПР4
 const popupEdit = document.querySelector('.popup_type_edit');
-const popupCloseButton = document.querySelector('.popup__close-btn');  //ПР4
+const profileCloseButton = document.querySelector('.popup__close-btn');  //ПР4
 const editButton = document.querySelector('.profile__edit-btn');  //ПР4
 const popupEditForm = document.querySelector('.popup__form_edit');  //ПР4
 const profileName = document.querySelector('.profile__name'); //ПР4
@@ -10,10 +9,13 @@ const profInput = document.querySelector('.popup__input_type_prof');  //ПР4
 const formAdd = document.querySelector('.popup__form_add');
 
 const addPopup = document.querySelector('.popup_type_add');
-const addPopupCloseButton = addPopup.querySelector('.popup__close-btn');  // кнопка закрытия окна
+const addProfileCloseButton = addPopup.querySelector('.popup__close-btn');  // кнопка закрытия окна
 const addButton = document.querySelector('.profile__add-btn');
 
 const popupImage = document.querySelector('.popup_image');   // фото Full Size
+
+const placeInput = addPopup.querySelector('.popup__input_type_place');
+const linkInput = addPopup.querySelector('.popup__input_type_link');
 
 
 // ПР5
@@ -39,24 +41,21 @@ editButton.addEventListener('click', () => {
 });   //ПР4
 
 
-const editButtons = document.querySelectorAll('.popup__close-btn');  // ищем все строки открытия
+const editButtons = document.querySelectorAll('.popup__close-btn');
 
-editButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const popupOpened = document.querySelector('.popup_opened');
-    closePopup(popupOpened);
-  });
+closeButtons.forEach((button) => {
+  // находим 1 раз ближайший к крестику попап
+  const popup = button.closest('.popup');
+  // устанавливаем обработчик закрытия на крестик
+  button.addEventListener('click', () => closePopup(popup));
 });
 
+popupEditForm.addEventListener('submit', editSubmitForm);   //ПР4
 
-popupEditForm.addEventListener('submit', editsubmitForm);   //ПР4
-
-function editsubmitForm(event) {
+function editSubmitForm(event) {
   event.preventDefault();  //ПР4
   profileName.textContent = nameInput.value;  //ПР4
   profileProf.textContent = profInput.value;  //ПР4
-  nameInput.value = profileName.textContent;  //ПР4
-  profInput.value = profileProf.textContent;  //ПР4
 
   closePopup(popupEdit);
 }
@@ -127,16 +126,6 @@ const initialCards = [
 
   })
 
-  const elements = document.querySelectorAll('.element__like');
-
- elements.forEach((element) => {
-  element.addEventListener('click', () => {
-    element.classList.toggle('element__like_active');
-   });
- });
-
-  return cardElements;
-};
 
 function preload() {
   const preloadArray = initialCards.map(card => createCard(card.name, card.link));
@@ -146,13 +135,10 @@ function preload() {
 function submitAddForm(event) {
   event.preventDefault();  // отменяем стандартное поведение отправки формы
 
-  const placeInput = addPopup.querySelector('.popup__input_type_place');
-  const linkInput = addPopup.querySelector('.popup__input_type_link');
-
   cardList.prepend(createCard(placeInput.value, linkInput.value));
 
   closePopup(addPopupForm);
-  // addPopupForm.reset();
+  evt.target.reset();
 }
 
 
@@ -160,9 +146,6 @@ addButton.addEventListener('click', () => {
   openPopup(addPopupForm);
 });  // открытие окна Новое место
 
-addPopupCloseButton.addEventListener('click', () => {
-  closePopup(addPopupForm);
-});  // закрытие окна Новое место
 
 formAdd.addEventListener('submit', submitAddForm);  // обработчик события отправки формы
 
